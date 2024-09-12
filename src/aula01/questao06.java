9dc0fc8d208da9caf7e69229d8b098650d87f43c
Nathan Felipi Almeida <nathanfelipi2006@gmail.com>
2024-09-12 19:53:43 -0300

    add questões

---
+++ src/aula01/questao06.java
@@ -0,0 +1,13 @@
+package aula01;
+
+public class questao06 {
+
+    public static void main(String[] args) {
+        double preco = 0.08;
+        int quantFolhas = 10;
+
+        System.out.println("Preço para pagar: R$" + preco * quantFolhas);
+
+    }
+
+}
